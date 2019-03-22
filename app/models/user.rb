88d6962ce8c6fadb792689abf222ede46de98d47
kypkyp <kyp@kmc.gr.jp>
2019-03-22 11:47:21 +0900

    implement remember in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,25 @@
 class User < ApplicationRecord
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 
+    attr_accessor :remember_token
+
     before_save {self.email.downcase!}
     validates :name, presence: true, length: {maximum: 50}
     validates :email, presence: true, length: {maximum: 255}, format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
     has_secure_password
     validates :password, presence: true, length: {minimum: 8}
 
-    def User.digest(string)
+    def self.digest(string)
       cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
       BCrypt::Password.create(string, cost: cost)
+    end
+
+    def self.new_token
+        SecureRandom.urlsafe_base64
+    end
+
+    def remember
+        self.remember_token = User.new_token
+        update_attribute(:remember_digest, User.digest(remember_token))
     end
 end
