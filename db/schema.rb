1157ef0e683b30a298a4e701a0f0aea92dd6a484
kypkyp <kyp@kmc.gr.jp>
2019-03-23 15:28:05 +0900

    finish remember/forget

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_22_024055) do
+ActiveRecord::Schema.define(version: 2019_03_23_060737) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 2019_03_22_024055) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
-    t.string "rememner_digest"
+    t.string "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
