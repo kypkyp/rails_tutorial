069df2d14ab73dd3d27fcde22d65743b00ee445d
kypkyp <kyp@kmc.gr.jp>
2019-03-25 15:05:10 +0900

    finish user edit

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
     validates :name, presence: true, length: {maximum: 50}
     validates :email, presence: true, length: {maximum: 255}, format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
     has_secure_password
-    validates :password, presence: true, length: {minimum: 8}
+    validates :password, presence: true, length: {minimum: 8}, allow_nil: true
 
     def self.digest(string)
       cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
