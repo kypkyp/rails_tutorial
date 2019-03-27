bb5ef460f9f4d5f387f79b2d10a9364e3dd716cf
kypkyp <kyp@kmc.gr.jp>
2019-03-27 23:17:25 +0900

    reset sampledata

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create!(name: "Example User", email:"example@railstutorial.org", password: "hogehoge", password_confirmation: "hogehoge")
+User.create!(name: "Example User", email:"example@railstutorial.org", password: "hogehoge", password_confirmation: "hogehoge", admin: true)
 
 99.times do |n|
   name = Faker::Name.name
