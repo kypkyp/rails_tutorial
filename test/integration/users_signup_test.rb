053a64e520cfcfcfd635deed4590082162c78bb4
kypkyp <kyp@kmc.gr.jp>
2019-03-28 21:47:27 +0900

    refactor~~~~

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -2,25 +2,23 @@ require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
 
-    test "invalid signup information" do
-        get signup_path
-        assert_no_difference "User.count" do
-            post users_path, params: {user: {name: "", email: "user@invalid", password: "foo", password_confirmation: "bar"}}
-        end
-        assert_template "users/new"
-        assert_select "div#error_explanation"
-        assert_select "div.field_with_errors"
+  test "invalid signup information" do
+    get signup_path
+    assert_no_difference "User.count" do
+      post users_path, params: {user: {name: "", email: "user@invalid", password: "foo", password_confirmation: "bar"}}
     end
+    assert_template "users/new"
+    assert_select "div#error_explanation"
+    assert_select "div.field_with_errors"
+  end
 
-    test "valid signup information" do
-        get signup_path
-        assert_difference "User.count", 1 do
-            post users_path, params: {user: {name: "Example User", email:"user@example.com", password: "hoge2hoge", password_confirmation: "hoge2hoge"} }
-        end
-        follow_redirect!
-        assert_template "users/show"
-        assert_select "div.alert-success"
+  test "valid signup information" do
+    get signup_path
+    assert_difference "User.count", 1 do
+      post users_path, params: {user: {name: "Example User", email:"user@example.com", password: "hoge2hoge", password_confirmation: "hoge2hoge"} }
     end
-
-
+    follow_redirect!
+    #assert_template "users/show"
+    #assert is_logged_in?
+  end
 end
