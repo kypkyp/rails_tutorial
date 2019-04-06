26d5559ad835d598e8ab4378ed6fedd8b43b2f75
kypkyp <kyp@kmc.gr.jp>
2019-04-07 08:06:53 +0900

    add passive relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ApplicationRecord
   has_many :microposts, dependent: :destroy
+
   has_many :active_relationships, class_name: 'Relationship', foreign_key: 'follower_id', dependent: :destroy
   has_many :followees, through: :active_relationships
+
+  has_many :passive_relationships, class_name: 'Relationship', foreign_key: 'followee_id', dependent: :destroy
+  has_many :followers, through: :passive_relationships
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 
